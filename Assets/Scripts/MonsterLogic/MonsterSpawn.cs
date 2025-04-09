89dff7c851df8ea4b2a8304e8dfcba519bd50110
tybnasgo <tybnasgo@naver.com>
2025-04-10 02:27:42 +0900

    몬스터가 회전되는 현상 수정, 몬스터 스폰 기본 로직

--- Assets/Scripts/MonsterLogic/MonsterSpawn.cs
+++ Assets/Scripts/MonsterLogic/MonsterSpawn.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class MonsterSpawn : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    MonsterFulling monsterFulling;
+    private void Awake()
+    {
+        monsterFulling = GetComponent<MonsterFulling>();
+    }
+    
     void Start()
     {
-        
+        MonsterSpawnMapEnter();
     }
 
-    // Update is called once per frame
-    void Update()
+    void MonsterSpawnMapEnter()
     {
-        
+        int genCount = 5;
+
+        for (int i = 0; i < genCount; i++)
+        {
+            int mobNum = Random.Range(0, 2);
+            GameObject gm = monsterFulling.MakeObj(mobNum);
+            gm.transform.position = new Vector3(Random.Range(-3,3),0,0);
+        }
     }
 }

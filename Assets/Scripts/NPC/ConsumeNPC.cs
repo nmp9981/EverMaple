820462059cc95b1212f703d68d30646150e7bd0d
tybnasgo <tybnasgo@naver.com>
2025-04-30 00:29:14 +0900

    상점 활성화 기능

--- Assets/Scripts/NPC/ConsumeNPC.cs
+++ Assets/Scripts/NPC/ConsumeNPC.cs
@@ -1,6 +1,62 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConsumeNPC : NPCCommon
 {
-    
+    [SerializeField]
+    Image playerImage;
+    [SerializeField]
+    Image npcImage;
+    [SerializeField]
+    TextMeshProUGUI mesoText;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        //base.Awake(); // 기반 클래스의 Awake()를 먼저 호출하여 초기화 진행
+
+        spriteRenderer = GameObject.Find("Player").GetComponent<SpriteRenderer>();
+        StoreButtonBinding();
+    }
+
+    protected override void SettingNPCImage()
+    {
+        base.SettingNPCImage();
+    }
+
+    private void OnEnable()
+    {
+        //플레이어 이미지 표시
+        playerImage.sprite = spriteRenderer.sprite;
+        //NPC이미지 표시
+        npcImage.sprite = npcSprite;
+        //메소 표시
+        mesoText.text = PlayerManager.PlayerInstance.PlayerMeso.ToString();
+
+
+    }
+
+    /// <summary>
+    /// 상점 버튼 바인딩
+    /// </summary>
+    void StoreButtonBinding()
+    {
+        foreach (Button btn in gameObject.GetComponentsInChildren<Button>())
+        {
+            string gmName = btn.name;
+            switch (gmName)
+            {
+                case "StoreExit":
+                    break;
+                case "ItemBuy":
+                    break;
+                case "ItemSell":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
--- Assets/Scripts/NPC/NPCCommon.cs
+++ Assets/Scripts/NPC/NPCCommon.cs
@@ -19,22 +19,65 @@ public class NPCCommon : MonoBehaviour
     [SerializeField]
     NPCCategory category;
 
-    GameObject consumeUI;
+    //UI 오브젝트
+    protected GameObject canvasUI;
+    protected GameObject consumeUI;
+    protected GameObject taxiUI;
+    protected GameObject questUI;
 
+    protected Sprite npcSprite;
+    private SpriteRenderer npcRenderer;
+
+    protected void Awake()
+    {
+        UIBinding();
+        SettingNPCImage();
+    }
+
+    protected virtual void SettingNPCImage()
+    {
+        npcRenderer = gameObject.GetComponent<SpriteRenderer>();
+        npcSprite = npcRenderer.sprite;
+    }
+  
     void OnMouseDown()
     {
-        Debug.Log(gameObject.name + " 이(가) 클릭되었습니다!");
+        switch (category)
+        {
+            case NPCCategory.Consume:
+            case NPCCategory.Equipment:
+                consumeUI.SetActive(true);
+                break;
+            case NPCCategory.Taxi:
+                taxiUI.SetActive(true);
+                break;
+            case NPCCategory.Quest:
+                questUI.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// UI바인딩
+    /// </summary>
+    void UIBinding()
+    {
+        canvasUI = GameObject.Find("Canvas");
 
         switch (category)
         {
             case NPCCategory.Consume:
-
-                break;
             case NPCCategory.Equipment:
+                consumeUI = canvasUI.transform.GetChild(5).gameObject;
+                npcRenderer = GetComponent<SpriteRenderer>();
                 break;
             case NPCCategory.Taxi:
+                taxiUI = canvasUI.transform.GetChild(6).gameObject;
                 break;
             case NPCCategory.Quest:
+                questUI = canvasUI.transform.GetChild(7).gameObject;
                 break;
             default:
                 break;

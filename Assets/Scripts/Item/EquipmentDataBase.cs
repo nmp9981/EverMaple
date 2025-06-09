be7992a27c6ae978b88fc6b5a9f53a548eaaae1c
tybnasgo <tybnasgo@naver.com>
2025-06-10 00:20:01 +0900

    장비 데이터 전체 추가

--- Assets/Scripts/Item/EquipmentDataBase.cs
+++ Assets/Scripts/Item/EquipmentDataBase.cs
@@ -224,18 +224,243 @@ public class EquipmentDataBase : MonoBehaviour
         ItemManager.itemInstance.equipmentItemDic.Add("흑몽", equiipmentOption38);
 
         EquiipmentOption equiipmentOption39
-        = new EquiipmentOption(equipmentImageList[39], EquipmentType.Hat, 14, 0, 20, 0, 35, "",
+        = new EquiipmentOption(equipmentImageList[39], EquipmentType.Up, 14, 0, 20, 0, 35, "",
         0, 0, 0, 0, 0, 1, 0, 0, 14, 0, 0, 0, 0, 0);
         ItemManager.itemInstance.equipmentItemDic.Add("흑야", equiipmentOption39);
 
         EquiipmentOption equiipmentOption40
-         = new EquiipmentOption(equipmentImageList[40], EquipmentType.Hat, 20, 0, 25, 0, 50, "",
+         = new EquiipmentOption(equipmentImageList[40], EquipmentType.Up, 20, 0, 25, 0, 50, "",
          0, 0, 0, 1, 0, 1, 0, 0, 18, 0, 0, 0, 0, 0);
         ItemManager.itemInstance.equipmentItemDic.Add("파오", equiipmentOption40);
 
+        EquiipmentOption equiipmentOption41
+         = new EquiipmentOption(equipmentImageList[41], EquipmentType.Up, 30, 0, 30, 0, 80, "",
+         0, 0, 0, 1, 0, 2, 0, 0, 27, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("스니크", equiipmentOption41);
 
+        EquiipmentOption equiipmentOption42
+         = new EquiipmentOption(equipmentImageList[42], EquipmentType.Up, 40, 0, 40, 0, 110, "",
+         0, 0, 0, 2, 0, 2, 0, 0, 36, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("스틸러", equiipmentOption42);
+
+        EquiipmentOption equiipmentOption43
+         = new EquiipmentOption(equipmentImageList[43], EquipmentType.Up, 50, 0, 50, 0, 140, "",
+         0, 0, 0, 3, 0, 3, 0, 0, 45, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("너클베스트", equiipmentOption43);
+
+        EquiipmentOption equiipmentOption44
+         = new EquiipmentOption(equipmentImageList[44], EquipmentType.Up, 60, 0, 60, 0, 170, "",
+         0, 0, 0, 3, 0, 4, 0, 0, 54, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("쉐도우", equiipmentOption44);
+
+        EquiipmentOption equiipmentOption45
+         = new EquiipmentOption(equipmentImageList[45], EquipmentType.Up, 70, 0, 70, 0, 200, "",
+         0, 0, 0, 4, 0, 4, 0, 0, 63, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("스콜피오", equiipmentOption45);
+
+        EquiipmentOption equiipmentOption46
+         = new EquiipmentOption(equipmentImageList[46], EquipmentType.Up, 80, 0, 75, 0, 220, "",
+         0, 0, 0, 4, 0, 5, 0, 0, 72, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("피라테", equiipmentOption46);
         #endregion
 
+        #region 하의
+        EquiipmentOption equiipmentOption47
+        = new EquiipmentOption(equipmentImageList[47], EquipmentType.Down, 0, 0, 0, 0, 0, "",
+        0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("청 반바지", equiipmentOption47);
+
+        EquiipmentOption equiipmentOption48
+        = new EquiipmentOption(equipmentImageList[48], EquipmentType.Down, 8, 0, 0, 0, 0, "",
+        0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("흑몽 바지", equiipmentOption48);
+
+        EquiipmentOption equiipmentOption49
+        = new EquiipmentOption(equipmentImageList[49], EquipmentType.Down, 14, 0, 20, 0, 35, "",
+        0, 0, 0, 0, 0, 0, 0, 0, 11, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("흑야 바지", equiipmentOption49);
+
+        EquiipmentOption equiipmentOption50
+         = new EquiipmentOption(equipmentImageList[50], EquipmentType.Down, 20, 0, 25, 0, 50, "",
+         0, 0, 0, 0, 0, 1, 0, 0, 15, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("파오 바지", equiipmentOption50);
+
+        EquiipmentOption equiipmentOption51
+         = new EquiipmentOption(equipmentImageList[51], EquipmentType.Down, 30, 0, 30, 0, 80, "",
+         0, 0, 0, 0, 0, 2, 0, 0, 22, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("스니크 바지", equiipmentOption51);
+
+        EquiipmentOption equiipmentOption52
+         = new EquiipmentOption(equipmentImageList[52], EquipmentType.Down, 40, 0, 40, 0, 110, "",
+         0, 0, 0, 1, 0, 2, 0, 0, 29, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("스틸러 바지", equiipmentOption52);
+
+        EquiipmentOption equiipmentOption53
+         = new EquiipmentOption(equipmentImageList[53], EquipmentType.Down, 50, 0, 50, 0, 140, "",
+         0, 0, 0, 1, 0, 3, 0, 0, 36, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("너클베스트 바지", equiipmentOption53);
+
+        EquiipmentOption equiipmentOption54
+         = new EquiipmentOption(equipmentImageList[54], EquipmentType.Down, 60, 0, 60, 0, 170, "",
+         0, 0, 0, 2, 0, 3, 0, 0, 43, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("쉐도우 바지", equiipmentOption54);
+
+        EquiipmentOption equiipmentOption55
+         = new EquiipmentOption(equipmentImageList[55], EquipmentType.Down, 70, 0, 70, 0, 200, "",
+         0, 0, 0, 2, 0, 4, 0, 0, 50, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("스콜피오 바지", equiipmentOption55);
+
+        EquiipmentOption equiipmentOption56
+         = new EquiipmentOption(equipmentImageList[56], EquipmentType.Down, 80, 0, 75, 0, 220, "",
+         0, 0, 0, 3, 0, 5, 0, 0, 57, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("피라테 바지", equiipmentOption56);
+        #endregion
+
+        #region 신발
+        EquiipmentOption equiipmentOption57
+        = new EquiipmentOption(equipmentImageList[57], EquipmentType.Shoes, 0, 0, 0, 0, 0, "",
+        0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("가죽 신발", equiipmentOption57);
+
+        EquiipmentOption equiipmentOption58
+        = new EquiipmentOption(equipmentImageList[58], EquipmentType.Shoes, 8, 0, 0, 0, 0, "",
+        0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("닌자 샌들", equiipmentOption58);
+
+        EquiipmentOption equiipmentOption59
+        = new EquiipmentOption(equipmentImageList[59], EquipmentType.Shoes, 14, 0, 20, 0, 35, "",
+        0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("에나멜 부츠", equiipmentOption59);
+
+        EquiipmentOption equiipmentOption60
+         = new EquiipmentOption(equipmentImageList[60], EquipmentType.Shoes, 20, 0, 25, 0, 50, "",
+         0, 0, 0, 0, 0, 1, 0, 0, 10, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("래피 부츠", equiipmentOption60);
+
+        EquiipmentOption equiipmentOption61
+         = new EquiipmentOption(equipmentImageList[61], EquipmentType.Shoes, 30, 0, 30, 0, 80, "",
+         0, 0, 0, 0, 0, 1, 0, 0, 15, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("아이젠", equiipmentOption61);
+
+        EquiipmentOption equiipmentOption62
+         = new EquiipmentOption(equipmentImageList[62], EquipmentType.Shoes, 40, 0, 40, 0, 110, "",
+         0, 0, 0, 0, 0, 2, 0, 0, 20, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("하프슈즈", equiipmentOption62);
+
+        EquiipmentOption equiipmentOption63
+         = new EquiipmentOption(equipmentImageList[63], EquipmentType.Shoes, 50, 0, 50, 0, 140, "",
+         0, 0, 0, 0, 0, 2, 0, 0, 25, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("고니슈즈", equiipmentOption63);
+
+        EquiipmentOption equiipmentOption64
+         = new EquiipmentOption(equipmentImageList[64], EquipmentType.Shoes, 60, 0, 60, 0, 170, "",
+         0, 0, 0, 1, 0, 3, 0, 0, 30, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("모스부츠", equiipmentOption64);
+
+        EquiipmentOption equiipmentOption65
+         = new EquiipmentOption(equipmentImageList[65], EquipmentType.Shoes, 70, 0, 70, 0, 200, "",
+         0, 0, 0, 1, 0, 3, 0, 0, 35, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("루티드슈즈", equiipmentOption65);
+
+        EquiipmentOption equiipmentOption66
+         = new EquiipmentOption(equipmentImageList[66], EquipmentType.Shoes, 80, 0, 75, 0, 220, "",
+         0, 0, 0, 2, 0, 4, 0, 0, 40, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("피레타부츠", equiipmentOption66);
+        #endregion
+
+        #region 장갑
+        EquiipmentOption equiipmentOption67
+        = new EquiipmentOption(equipmentImageList[67], EquipmentType.Glove, 10, 0, 0, 0, 0, "",
+        0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("노가다 목장갑", equiipmentOption67);
+
+        EquiipmentOption equiipmentOption68
+         = new EquiipmentOption(equipmentImageList[68], EquipmentType.Glove, 20, 0, 25, 0, 50, "",
+         0, 0, 0, 0, 0, 1, 1, 0, 6, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("스틸라인", equiipmentOption68);
+
+        EquiipmentOption equiipmentOption69
+         = new EquiipmentOption(equipmentImageList[69], EquipmentType.Glove, 30, 0, 30, 0, 80, "",
+         0, 0, 0, 0, 0, 1, 2, 0, 10, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("실비아", equiipmentOption69);
+
+        EquiipmentOption equiipmentOption70
+         = new EquiipmentOption(equipmentImageList[70], EquipmentType.Glove, 40, 0, 40, 0, 110, "",
+         0, 0, 0, 0, 0, 2, 3, 0, 14, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("클리브", equiipmentOption70);
+
+        EquiipmentOption equiipmentOption71
+         = new EquiipmentOption(equipmentImageList[71], EquipmentType.Glove, 50, 0, 50, 0, 140, "",
+         0, 0, 0, 0, 0, 2, 4, 0, 18, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("황월장갑", equiipmentOption71);
+
+        EquiipmentOption equiipmentOption72
+         = new EquiipmentOption(equipmentImageList[72], EquipmentType.Glove, 60, 0, 60, 0, 170, "",
+         0, 0, 0, 0, 0, 3, 5, 0, 22, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("파우", equiipmentOption72);
+
+        EquiipmentOption equiipmentOption73
+         = new EquiipmentOption(equipmentImageList[73], EquipmentType.Glove, 70, 0, 70, 0, 200, "",
+         0, 0, 0, 0, 0, 3, 6, 0, 26, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("와이어스", equiipmentOption73);
+
+        EquiipmentOption equiipmentOption74
+         = new EquiipmentOption(equipmentImageList[74], EquipmentType.Glove, 80, 0, 75, 0, 220, "",
+         0, 0, 0, 0, 0, 4, 7, 0, 30, 0, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("로버", equiipmentOption74);
+        #endregion
+
+        #region 귀고리
+        EquiipmentOption equiipmentOption75
+        = new EquiipmentOption(equipmentImageList[75], EquipmentType.Earing, 10, 0, 0, 0, 0, "",
+        0, 0, 0, 0, 1, 0, 0, 0, 0, 5, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("추 귀고리", equiipmentOption75);
+
+        EquiipmentOption equiipmentOption76
+         = new EquiipmentOption(equipmentImageList[76], EquipmentType.Earing, 20, 0, 0, 0, 0, "",
+         0, 0, 0, 0, 2, 0, 0, 0, 0, 10, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("번개 귀고리", equiipmentOption76);
+
+        EquiipmentOption equiipmentOption77
+         = new EquiipmentOption(equipmentImageList[77], EquipmentType.Earing, 30, 0, 0, 0, 0, "",
+         0, 0, 0, 0, 3, 0, 0, 0, 0, 15, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("사파이어 귀고리", equiipmentOption77);
+
+        EquiipmentOption equiipmentOption78
+         = new EquiipmentOption(equipmentImageList[78], EquipmentType.Earing, 40, 0, 0, 0, 0, "",
+         0, 0, 0, 0, 4, 0, 0, 0, 0, 20, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("블루 문", equiipmentOption78);
+
+        EquiipmentOption equiipmentOption79
+         = new EquiipmentOption(equipmentImageList[79], EquipmentType.Earing, 50, 0, 0, 0, 0, "",
+         0, 0, 0, 0, 5, 0, 0, 0, 0, 25, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("해골 귀고리", equiipmentOption79);
+
+        EquiipmentOption equiipmentOption80
+         = new EquiipmentOption(equipmentImageList[80], EquipmentType.Earing, 60, 0, 0, 0, 0, "",
+         0, 0, 0, 0, 6, 0, 0, 0, 0, 30, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("크리스탈 플라워링", equiipmentOption80);
+
+        EquiipmentOption equiipmentOption81
+         = new EquiipmentOption(equipmentImageList[81], EquipmentType.Earing, 70, 0, 0, 0, 0, "",
+         0, 0, 0, 0, 7, 0, 0, 0, 0, 35, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("메이플 이어링", equiipmentOption81);
+
+        EquiipmentOption equiipmentOption82
+         = new EquiipmentOption(equipmentImageList[82], EquipmentType.Earing, 80, 0, 0, 0, 0, "",
+         0, 0, 0, 0, 8, 0, 0, 0, 0, 40, 0, 0, 0, 0);
+        ItemManager.itemInstance.equipmentItemDic.Add("소드 이어링", equiipmentOption82);
+
+        EquiipmentOption equiipmentOption83
+         = new EquiipmentOption(equipmentImageList[83], EquipmentType.Cape, 25, 0, 0, 0, 0, "",
+         0, 0, 1, 1, 1, 1, 0, 0, 6, 12, 0, 0, 0, 10);
+        ItemManager.itemInstance.equipmentItemDic.Add("허름한 망토", equiipmentOption83);
+
+        EquiipmentOption equiipmentOption84
+         = new EquiipmentOption(equipmentImageList[84], EquipmentType.Cape, 42, 0, 0, 0, 0, "",
+         0, 0, 2, 2, 2, 2, 0, 0, 12, 18, 0, 3, 0, 15);
+        ItemManager.itemInstance.equipmentItemDic.Add("이카루스의 망토", equiipmentOption84);
+        #endregion
     }
     #endregion
 }
